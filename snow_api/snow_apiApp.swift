46bc87d218f36b352e09f9918491dd97fec72c0a
Hyeonhee <hyunheeshim0103@gmail.com>
2024-01-08 22:29:25 +0900

    Initial Commit

---
+++ snow_api/snow_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  snow_apiApp.swift
+//  snow_api
+//
+//  Created by 심현희 on 2024/01/08.
+//
+
+import SwiftUI
+
+@main
+struct snow_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
